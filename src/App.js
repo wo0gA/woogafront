7e4ab42387d3fa6092e9e9890a64f0739b1bcda3
suzzang2 <subin030102@naver.com>
2024-07-28 14:23:55 +0900

    ADD: 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import GoodsDetailPage from './pages/GoodsDetailPage';
 import MainPage from './pages/MainPage';
 import { Routes, Route } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/goodsDetail" element={<GoodsDetailPage />} />
       </Routes>
     </div>
 
---
+++ src/pages/GoodsDetailPage.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import React from 'react'
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+import GoodsDetailMain from "../components/GoodsDetailMain";
+
+const GoodsDetailPage = () => {
+  return (
+    <Wrapper>
+      <Header>
+        
+      </Header> 
+      <GoodsDetailMain />
+      <Footer>
+        
+      </Footer>
+    </Wrapper>
+  )
+}
+
+export default GoodsDetailPage
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+
+  /* padding-left: 100px;
+  padding-right: 100px; */
+`;
