4943c7e81c9857f62ed0e7cc532f423a102d6386
suzzang2 <subin030102@naver.com>
2024-07-29 09:36:12 +0900

    CHORE: 의존성 proxy 삭제 /for npm start

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Routes>
       <Route path="/" element={<MainPage />} />
-        <Route path="myPage" element={<MyPage />} />
+        <Route path="/myPage" element={<MyPage />} />
         <Route path="/goodsDetail" element={<GoodsDetailPage />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
