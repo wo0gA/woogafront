24251abb3de766e686ebae20dbb3ea39120f5a08
joeyycho <joeycho@cau.ac.kr>
2024-07-29 21:45:28 +0900

    GIT: App.js 충돌 수정

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import MainPage from './pages/MainPage';
 import { Routes, Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
 import MyPage from './pages/MyPage';
-import RegistrationPage from './pages/RegistrationPage';
+import TestPage from './pages/TestPage';
+import Chatting from './apis/Chatting';
 
 function App() {
 
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<MainPage />} />
-        <Route path="myPage" element={<MyPage />} />
+        {/* 일단 바꿔둠 */}
+      <Route path="/" element={<TestPage />} /> 
+        <Route path="/myPage" element={<MyPage />} />
         <Route path="/goodsDetail" element={<GoodsDetailPage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/chatting" element={<Chatting />} />
       </Routes>
     </div>
 
