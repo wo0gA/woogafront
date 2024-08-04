ead2299723cad132451d1a5d19a56b78a9ee31e8
동영 김 <kimarang0@naver.com>
2024-08-05 00:08:12 +0900

    feat: 카테고리 api 연결 완료

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,43 +1,74 @@
 import styled from "styled-components";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RentalCategoryPage from "../components/RentalCategoryPage";
 import CategoryComponent from "../components/CategoryComponent";
+import axios from "axios";
+
+const SERVER_URL = "server.templ.es";
 
 const CategoryPage = () => {
-    const [selectedItem, setSelectedItem] = useState(null);
+	const navigate = useNavigate();
+	const location = useLocation();
 
-    const handleItemSelect = (item) => {
-      setSelectedItem(item);
-    };
-    
-  return (
-    <Wrapper>
-      <Header>
-        
-      </Header> 
-    
-      <CategoryComponent onItemSelect={handleItemSelect} />
-      {selectedItem && <RentalCategoryPage selectedItem={selectedItem} />}
-      <Footer>
-       
-      </Footer>
-    </Wrapper>
-  )
-}
+	const [selectedItem, setSelectedItem] = useState(null);
+	const [selectedProducts, setSelectedProducts] = useState([]);
 
-export default CategoryPage
+	const updateCategoryParam = (categoryName) => {
+		const searchParams = new URLSearchParams(location.search);
+		searchParams.set("category", categoryName);
+		navigate(`?${searchParams.toString()}`);
+	};
+
+	useEffect(() => {
+		const searchParams = new URLSearchParams(location.search);
+		const categoryParam = searchParams.get("category");
+		const keywordParam = searchParams.get("keyword");
+		if (categoryParam) {
+			fetchProducts("category", categoryParam);
+			setSelectedItem(categoryParam);
+		} else if (keywordParam) {
+			fetchProducts("keyword", keywordParam);
+			setSelectedItem(keywordParam);
+		}
+	}, [location.search]);
+
+	const fetchProducts = async (param, categoryName) => {
+		const trimmedName = categoryName.replaceAll(" ", "");
+		try {
+			const response = await axios.get(
+				`https://${SERVER_URL}/products/?${param}=${trimmedName}`
+			);
+			setSelectedProducts(response.data);
+		} catch (err) {
+			setSelectedProducts([]);
+		}
+	};
+
+	return (
+		<Wrapper>
+			<Header></Header>
+
+			<CategoryComponent onItemSelect={updateCategoryParam} />
+			{selectedItem && <RentalCategoryPage selectedItem={selectedItem} />}
+			<Footer></Footer>
+		</Wrapper>
+	);
+};
+
+export default CategoryPage;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	box-sizing: border-box;
 
-  /* padding-left: 100px;
+	/* padding-left: 100px;
   padding-right: 100px; */
 `;
