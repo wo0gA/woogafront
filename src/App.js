c7b9244b66284658e077f5bdccde2500a72a5d0b
joeyycho <joeycho@cau.ac.kr>
2024-08-05 02:12:10 +0900

    GIT: 충돌 해결

--- src/App.js
+++ src/App.js
@@ -11,29 +11,33 @@ import CategoryPage from './pages/CategoryPage';
 import ChatPage from "./pages/ChatPage";
 import RegistrationPage from './pages/RegistrationPage';
 import { RentalFeeProvider } from './context/RentalFeeContext';
-import RegistrationDetailPage from './pages/RegistrationDetailPage'
+import MagazinePage from './pages/MagazinePage';
+import StorePage from './pages/StorePage';
+import { NavProvider } from './context/NavContext';
 
 function App() {
 
   return (
     <RentalFeeProvider>
+      <NavProvider>
       <div className="App">
         <Routes>
           <Route path="/" element={<MainPage />} /> 
           <Route path="/main" element={<MainPage />} />
           <Route path="/myPage" element={<MyPage />} />
-          <Route path="/goodsDetail" element={<GoodsDetailPage />} />
+          <Route path="/goodsDetail/:productID" element={<GoodsDetailPage />} />
           <Route path="/rentalCategory" element={<CategoryPage />} />
           <Route path="/test" element={<TestPage />} />
-{/*          <Route path="/chatting" element={<ChatPage />} />
-*/}          <Route path="/nearby" element={<NearbyPage />} />
+          <Route path="/chatting" element={<ChatPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profileSetting" element={<ProfileSettingPage />} />
           <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/registrationdetail" element={<RegistrationDetailPage/>} />
           <Route path='/loading' element={<></>} /> 
+          <Route path='/magazine' element={<MagazinePage />} />
+          <Route path='/store' element={<StorePage/>} />
         </Routes>
       </div>
+      </NavProvider>
     </RentalFeeProvider>
 
   );
