47a06d8b2555591d3b8ddc61fe589327acf98fd0
suzzang2 <subin030102@naver.com>
2024-08-06 18:45:35 +0900

    FEAT: 키워드 검색해도 새로고침되지 않게 수정

--- src/apis/GoogleLoginButton.jsx
+++ src/apis/GoogleLoginButton.jsx
@@ -4,19 +4,19 @@ import googleBtn from '../images/googleBtn.png';
 import { useLocation } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    let REDIRECT_URI = '';
-    //local 환경이면 localhost:3000, 배포 환경이면 배포 주소
-    //searchParams에 localhost:3000이 포함되어 있으면 local 환경
-    if (searchParams.has('localhost')) {
-        REDIRECT_URI = 'http://localhost:3000/login';
-    }
-    else {
-        REDIRECT_URI = 'https://server.templ.es/login';
-    }
+    // const location = useLocation();
+    // const searchParams = new URLSearchParams(location.search);
+    // let REDIRECT_URI = '';
+    // //local 환경이면 localhost:3000, 배포 환경이면 배포 주소
+    // //searchParams에 localhost:3000이 포함되어 있으면 local 환경
+    // if (searchParams.has('localhost')) {
+    //     REDIRECT_URI = 'http://localhost:3000/login';
+    // }
+    // else {
+    //     REDIRECT_URI = 'https://server.templ.es/login';
+    // }
 
-    // const REDIRECT_URI = 'http://localhost:3000/login';
+    const REDIRECT_URI = 'http://localhost:3000/login';
     const CLIENT_ID = '564600149423-3k57skta0kr16fblavp23vp34k9tvkp0.apps.googleusercontent.com';
     const GOOGLE_LOGIN_LINK = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email%20profile`;
 
